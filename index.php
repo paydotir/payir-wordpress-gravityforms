b2492b52d3a9eb2e25e64f3f863069d5db0a01a7
MehrdadJalebi <mehrdad.jalebi@gmail.com>
2018-09-03 14:09:22 +0430

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: درگاه پی دات آی آر گرویتی فرم
+Plugin URI: http://gravityforms.ir/
+Description: افزونه درگاه پرداخت پی دات آی آر برای فرم ساز فوق پیشرفته Gravity Forms
+Version: 2.3.0
+Author: گرویتی فرم پارسی
+Author URI: http://gravityforms.ir/
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+require_once( 'pay.php' );
